bd3a58f39d8301684e6aeb9d5b49d4326d251cec
omar-mesto <omisto014@gmail.com>
2024-12-14 16:23:21 +0300

    add zod-i18n Library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    'nuxt-zod-i18n',
+    '@nuxtjs/i18n'
+  ],
   css: ['~/assets/css/main.css'],
   typescript: {
     typeCheck: true
