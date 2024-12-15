3388ca9e7309cc33b200c233a99113845ad6b15f
omar-mesto <omisto014@gmail.com>
2024-12-16 02:51:03 +0300

    update Lang Component

--- components/shared/lang.vue
+++ components/shared/lang.vue
@@ -1,13 +1,12 @@
 <template>  
     <UApp>  
-      <button @click="toggleLocale">{{ locale === 'ar' ? 'en' : 'ar' }}</button> 
+      <UButton @click="toggleLocale">{{ locale === 'ar' ? 'en' : 'ar' }}</UButton> 
     </UApp>  
   </template>  
   
   <script setup lang="ts">  
-  import { ref } from 'vue';  
-  import { useI18n } from 'vue-i18n';  
-  const { t, locale, setLocale } = useI18n();  
+  import { useI18n } from 'vue-i18n';
+  const { locale, setLocale } = useI18n();  
   
   const toggleLocale = () => {
     setLocale(locale.value === 'ar' ? 'en' : 'ar');
