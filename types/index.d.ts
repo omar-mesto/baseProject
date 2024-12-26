1ec4068290661cf7a2cd6e68db020391e76e4749
omar-mesto <omisto014@gmail.com>
2024-12-26 16:36:50 +0300

    add Interface to Login Form

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,12 @@
 declare global {
-   
+      interface LoginForm{
+        email:string,
+        password:string
+      }  
+      type ToasterColors = {
+        sucess:"success",
+        error:"error"
+      } 
   }
   
   export { };
